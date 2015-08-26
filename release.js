53f92b65812a0b97508660f84f2128cf88617b30
zoltantarcsay <tarcsay.zoltan@gmail.com>
2015-08-26 15:51:04 +0100

    first commit

---
+++ release.js
@@ -0,0 +1,85 @@
+var args = process.argv.slice(2),
+    exec = require('child_process').exec,
+    fs = require('fs'),
+    semver = require('semver'),
+    promise = require('promise'),
+    manifests = [{path: './package.json'}, {path: './bower.json'}],
+    newVersion;
+
+function updateVersions() {
+    manifests.forEach(function (manifest) {
+        manifest.data = require(manifest);
+        manifest.oldVersion = manifest.data.version;
+    });
+
+    newVersion = semver.valid(args[0]) || semver.inc(manifests[0].data.version, args[0]);
+
+    manifests.forEach(function (manifest) {
+        manifest.data.version = newVersion;
+    });
+
+    if (!args[0])
+        return Promise.reject(manifests.map(function (manifest) {
+            return [manifest.path, manifest.oldVersion];
+        }));
+
+    return Promise.all(manifests.map(function (manifest) {
+        return new Promise(function (resolve, reject) {
+            fs.wirteFile(manifest.path, JSON.stringify(manifest.data, null, 2), function (err) {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }));
+}
+
+function cmd(command) {
+    return new Promise(function (resolve, reject) {
+        exec(command, function (err, stdout, stderr) {
+            if (err) return reject(err);
+            if (stderr) return reject(stderr);
+            console.log(stdout);
+            resolve(stdout);
+        });
+    });
+}
+
+function checkout(branch) {
+    return cmd('git checkout ' + branch);
+}
+
+function commit(message) {
+    return cmd('git commit -a -m ' + message);
+}
+
+function push() {
+    return cmd('git push');
+}
+
+function merge(fromBranch) {
+    return cmd('git merge ' + fromBranch);
+}
+
+checkout('master')
+    .then(updateVersions)
+    .then(function () {
+        return commit('v' + newVersion);
+    })
+    .then(function () {
+        return push();
+    })
+    .then(function () {
+        return checkout('release/beta');
+    })
+    .then(function () {
+        return merge('master');
+    })
+    .then(function () {
+        return checkout('release/production');
+    })
+    .then(function () {
+        return merge('release/beta');
+    })
+    .catch(function (err) {
+        console.err(err);
+    });
